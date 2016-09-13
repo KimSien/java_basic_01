cbb020d0e200333bbe56226a2a5095fa55b99569
tksok2009 <tksok2009@gmail.com>
2016-09-13 16:28:43 +0900

    test function

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -6,6 +6,16 @@ public class hello {
 		int[] number = {10,20,30,40,50};
 		for(int i = 0; i < number.length; i++){
 		System.out.println(number[i]);	}
-	}
 		
+		addplus(2);
+	}
+
+	
+	private static int addplus(int a){
+		int b;
+		b = a;
+		System.out.println(a);
+		return b;
+	}
+	
 }
