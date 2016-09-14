d0c545b49d3de4e989f55fff536cd51cec8a0231
toysking <tksok2009@gmail.com>
2016-09-15 08:04:55 +0900

    reset fist cording. draft source set. and reject eclispe workspace direcotry with .metadata

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -3,29 +3,7 @@ package hello;
 public class hello {
 
 	public static void main(String[] args) {
-		int[] number = {10,20,30,40,50};
-		for(int i = 0; i < number.length; i++){
-		System.out.println(number[i]);	}
-		
-		
-		//test function
-		int c;
-		c = addplus(2);
-		System.out.println(c);
-		
-		
-		//test class
-		
-		test tests = new test();
-		tests.testprint();
-	}
-
-	
-	private static int addplus(int a){
-		int b;
-		b = a;
-		System.out.println(a);
-		return b;
+		System.out.println("base");	}
 	}
 	
 }
